96cbd25365dd41b34cc4ec129dff9fa32855c647
Billy-PC\Billy <fran922003@yahoo.com>
2016-10-18 22:06:47 -0400

    BT04 Creating & Deleting Landscapes

---
+++ BTank/Source/BTank/BTankGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BTankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BTANK_API ABTankGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
