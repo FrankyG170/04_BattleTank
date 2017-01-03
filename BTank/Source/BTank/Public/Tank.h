14c1f5d7f95c9dfc1293fc85ed45b88698aa1d06
Billy-PC\Billy <fran922003@yahoo.com>
2017-01-02 22:43:04 -0500

    BT024 Spawning Projectiles, Fixing collision bug,  Added new assets

--- BTank/Source/BTank/Public/Tank.h
+++ BTank/Source/BTank/Public/Tank.h
@@ -10,6 +10,7 @@
 class UTankBarrel;
 class UTankTurret;
 class UTankAimingComponent;
+class AProjectile;
 
 UCLASS()
 class BTANK_API ATank : public APawn
@@ -17,13 +18,17 @@ class BTANK_API ATank : public APawn
 	GENERATED_BODY()
 
 public:
-	void AimAt(FVector HitLocation);
-
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetBarrelReference(UTankBarrel* BarrelToSet);
 
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetTurretReference(UTankTurret* TurretToSet);
+
+	void AimAt(FVector HitLocation);
+
+	// Alows player to fire
+	UFUNCTION(BlueprintCallable, Category = Firing)
+	void Fire();
 
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
@@ -40,5 +45,10 @@ private:
 
 	UPROPERTY(EditAnywhere, Category = Firing)
 	float LaunchSpeed = 100000; // TODO find sensible default
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+	TSubclassOf<AProjectile> ProjectileBlueprint;
+
+	UTankBarrel* Barrel = nullptr; // Local barrel reference for spawning projectile
 	
 };
